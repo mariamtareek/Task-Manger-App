648c51bc7e8362bb0b88a7eb90fc970a48417c1f
mariamtareek <mariamtarek5015@gmail.com>
2025-09-25 02:45:04 +0300

    feat: base skeleton component

---
+++ task-manger/src/components/shared/Skeleton.vue
@@ -0,0 +1,21 @@
+<template>
+        <div class="bg-gray-200 rounded-md w-full h-full animatePulse"></div>
+</template>
+
+<style scoped>
+@keyframes pulse {
+    0% {
+        background-color: #e0e0e0;
+    }
+    50% {
+        background-color: #f0f0f0;
+    }
+    100% {
+        background-color: #e0e0e0;
+    }
+}
+
+.animatePulse {
+    animation: pulse 1.5s infinite ease-in-out;
+}
+</style>
